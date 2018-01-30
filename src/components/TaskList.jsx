65baa664e5b1482d16916d896801ce0da9f9bcca
Gaël Hanquez <Kribou@users.noreply.github.com>
2018-01-30 16:02:56 +0100

    Add tasklist

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import FaCheckSquareO from 'react-icons/lib/fa/check-square-o';
+import styled from 'styled-components';
+import theme from '../config/theme';
+
+/* {
+  status: 'PENDING',
+    date: '3 min ago',
+      description:
+  'The monkey-rope is found in all whalers; but it was only in the Pequod that the monkey...',
+    progress: 0
+}, */
+
+class Status extends React.Component {
+  render() {
+    const { status } = this.props;
+    const StatusPanel = styled.div`
+      background-color: ${status === 'PENDING' ? 'red' : 'yellow'};
+      color: black;
+    `;
+    return <StatusPanel>{status}</StatusPanel>;
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    const { status, date } = this.props.task;
+    const Panel = styled.div`
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      margin-left: 20px;
+    `;
+    return (
+      <Panel>
+        <Status status={status} />
+        {date}
+      </Panel>
+    );
+  }
+}
+
+class Description extends React.Component {
+  render() {
+    const { description = '' } = this.props;
+    const Panel = styled.div`color: ${theme.searchInput.color};`;
+    return <Panel>{description}</Panel>;
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+}
+
+class ProgressBar extends React.Component {
+  render() {
+    const { progress } = this.props;
+    return <div>{progress}</div>;
+  }
+}
+
+export default class TaskList extends React.Component {
+  render() {
+    const { tasks } = this.props;
+
+    return (
+      <div>
+        {tasks.map((task, idx) => {
+          const { progress, description, status, date } = task;
+          return (
+            <div key={idx}>
+              <Header task={task} />
+              <Description description={description} />
+              <Footer>
+                <ProgressBar progress={progress} />
+                <FaCheckSquareO />
+              </Footer>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,9 @@
-import * as React from "react";
-import styled from "styled-components";
-import SearchInput from "./SearchInput";
-import NavBar from "./NavBar";
-import Tab from "./Tab";
+import * as React from 'react';
+import styled from 'styled-components';
+import SearchInput from './SearchInput';
+import NavBar from './NavBar';
+import Tab from './Tab';
+import TaskList from './TaskList';
 
 const Panel = styled.div`
   height: 100%;
@@ -17,6 +18,30 @@ const SearchInputStyle = styled.div`
   margin-right: 20px;
 `;
 
+const tasks = [
+  {
+    status: 'PENDING',
+    date: '3 min ago',
+    description:
+      'The monkey-rope is found in all whalers; but it was only in the Pequod that the monkey...',
+    progress: 0
+  },
+  {
+    status: 'DELAYED',
+    date: '3 min ago',
+    description:
+      'The monkey-rope is found in all whalers; but it was only in the Pequod that the monkey...',
+    progress: 94
+  },
+  {
+    status: 'IN PROGRESS',
+    date: '3 min ago',
+    description:
+      'The monkey-rope is found in all whalers; but it was only in the Pequod that the monkey...',
+    progress: 34
+  }
+];
+
 export default class Tasks extends React.Component {
   render() {
     return (
@@ -29,6 +54,7 @@ export default class Tasks extends React.Component {
           <Tab>In Progress</Tab>
           <Tab>Projects</Tab>
         </NavBar>
+        <TaskList tasks={tasks} />
       </Panel>
     );
   }
