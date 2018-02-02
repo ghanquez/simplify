78a79c4db4a37476740851c35a5afe3b1019a20d
Gaël Hanquez <Kribou@users.noreply.github.com>
2018-02-02 22:02:50 +0100

    Add dropdown button from bootstrap

---
+++ src/data/members.js
@@ -0,0 +1,8 @@
+const imgRootPath =
+  "https://rawgit.com/Kribou/simplify/66983ce6e4242211f2b05d1417435c35b26145d4/src/img/";
+
+const members = ["1033", "694", "565", "3", "468"].map(d => {
+  return `${imgRootPath}/${d}.jpg`;
+});
+
+export default members;
